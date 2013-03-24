78b3a2466f9257311b9797befffe71f41a5b901f
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-03-23 18:39:16 -0700

    adding more test

--- test/distance.js
+++ test/distance.js
@@ -21,10 +21,28 @@ describe("Distance", function(){
 		it("should set the starts first value to the same value as argument and parse a float", function(){
 			assert.equal(123.12, Distance().from(["123.12",1212312]).start[0])
 		})
+		it("should set the starts second value to the same value as argument and parse a float", function(){
+			assert.equal(1212312, Distance().from(["123.12","1212312"]).start[1])
+		})
 	})
 	describe(".to()", function(){
-		it("should return a number if from is called before", function(){
+		it("should return an instance of distance if its called before .from()", function(){
+			assert.equal(true, Distance().to([234234,23423]) instanceof Distance)
+		})
+		it("should return a number if .from() is called before", function(){
 			assert.equal("number", typeof Distance().from([12312,1212312]).to([234234,23423]))
+		})
+		it("should set the destinations first value to the same value as argument", function(){
+			assert.equal(12312, Distance().to([12312,1212312]).destination[0])
+		})
+		it("should set the destinations second value to the same value as argument", function(){
+			assert.equal(1212312, Distance().to([12312,1212312]).destination[1])
+		})
+		it("should set the destinations first value to the same value as argument and parse a float", function(){
+			assert.equal(123.12, Distance().to(["123.12",1212312]).destination[0])
+		})
+		it("should set the starts second value to the same value as argument and parse a float", function(){
+			assert.equal(1212312, Distance().to(["123.12","1212312"]).destination[1])
 		})
 	})
 })
