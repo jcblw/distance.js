d8f684f4d2c6e07fc2a4674db42de53d2c1af97a
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-04-03 21:25:55 -0700

    making a units a construct to make it alot less namspacey

--- src/units.js
+++ src/units.js
@@ -6,38 +6,43 @@
 
 (function(exports){
 
-	// need to pull these out and have only one object
-	// modifing the Number prototype
+  var Units = function(base){
+    if(!(this instanceof Units)){
+      return new Units(base);
+    }
+    var that = this;
+    this.base = parseFloat(base);
 
-	Number.prototype.kilometer = function(){
-		this.converted = true;
-		return this.valueOf();
-	};
+    this.units = {};
+    this.units.kilometer = function(){
+      return that.base;
+    };
+    this.units.miles = function(){
+      return that.base * 0.621371;
+    };
+    this.units.feet = function(){
+      return that.base * 3280.84;
+    };
 
-	Number.prototype.miles = function(){
-		this.converted = true;
-		return this.valueOf() * 0.621371;
-	};
+  };
 
-	Number.prototype.feet = function(){
-		this.converted = true;
-		return this.valueOf() * 3280.84;
-	};
+  var attach = function(that){
+    that._convertUnit = Units(that.valueOf());
+  };
 
-	Number.prototype.unit = function(unit){
-		if(this[unit] && !this.converted){
-			return this[unit]();
-		}else{
-			throw new Error("One one conversion is possible at this time");
-		}
-	};
+  Number.prototype.unit = function(unit){
+    if(!this._convertUnit){
+      attach(this);
+    }
+    return this._convertUnit.units[unit]();
+  };
 
-	Array.prototype.unit = function(unit){
-		var result = [];
-		for(var i = 0; i < this.length; i += 1){
-			result.push(parseFloat(this[i]).unit(unit));
-		}
-		return result;
-	};
+  Array.prototype.unit = function(unit){
+    var result = [];
+    for(var i = 0; i < this.length; i += 1){
+      result.push(parseFloat(this[i]).unit(unit));
+    }
+    return result;
+  };
 
 }(this));
